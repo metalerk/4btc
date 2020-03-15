ff448b1ed0c04a009803004d66723d3f1646111d
Luis Esteban <rodriguezjluis0@gmail.com>
2020-03-15 02:53:32 -0600

    fix math bug

--- btc_tracker_engine/helper_functions.py
+++ btc_tracker_engine/helper_functions.py
@@ -1,5 +1,5 @@
 def rate_diff_percentage(previous_rate, current_rate, percentage=False):
-    diff_percentage = (previous_rate - current_rate) / previous_rate
+    diff_percentage = (current_rate - previous_rate) / previous_rate
     if percentage:
         return diff_percentage * 100
     return diff_percentage
