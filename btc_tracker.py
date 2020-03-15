4170fbf743e3f4b1ca8f03eb6874bf0d4a2efc06
Luis Esteban <rodriguezjluis0@gmail.com>
2020-03-15 04:08:28 -0600

    fix bugs

--- btc_tracker.py
+++ btc_tracker.py
@@ -1,3 +1,4 @@
+from datetime import datetime as dt
 import sys
 import time
 import click
@@ -41,24 +42,34 @@ def main(currency, interval, alert, min_rate, max_rate, rate_window, times):
             print(f'[-] Invalid currency \'{currency}\'.\n')
             sys.exit(1)
         previous_rate = btc_tracker.get_currency_price(currency, just_rate=True)['rate']
+        first_rate = previous_rate
+        print('[!] - Tracking from CoinDesk -')
+        print(f"[-] INITIAL: ${first_rate:,} {currency}   ====   {dt.now().strftime('%a %d-%m-%Y %H:%M:%S%f %Z')}\n\n")
         _flag = True
         counter = 0
         previous_rate_percentage = 1
         while(_flag):
             obj = btc_tracker.get_currency_price(currency)
             increase_rate_percentage = rate_diff_percentage(previous_rate, obj['rate'], percentage=True)
-            previous_rate = obj['rate']
+            first_rate_percentage = rate_diff_percentage(first_rate, obj['rate'], percentage=True)
             if increase_rate_percentage != 0 and increase_rate_percentage != previous_rate_percentage:
-                updown_indicator = '+' if increase_rate_percentage >= 0 else '-'
-                increase_decrease_indicator = 'INCREASE' if updown_indicator == '+' else 'DECREASE'
-                print(f"[{updown_indicator}] {obj['timestamp'].strftime('%a %d-%m-%Y %H:%M:%S%f %Z')} --- 1 BTC => ${obj['rate']:,} {obj['currency_code']} - {increase_decrease_indicator} RATE  % {increase_rate_percentage}")
-                print('·····························································································')
+                updown_indicator = lambda x: '+' if x >= 0 else '-'
+                updown = updown_indicator(increase_rate_percentage)
+                print(f"[{updown}] {obj['timestamp'].strftime('%a %d-%m-%Y %H:%M:%S%f %Z')}\n")
+                print(f"[{updown}] INITIAL RATE: ${first_rate:,} {currency}")
+                print(f"[{updown}] CURRENT RATE: ${obj['rate']:,} {currency}")
+                print(f"[{updown}] CURRENT PROFIT: ${(obj['rate'] - previous_rate):,} {currency}")
+                print(f"[{updown}] GLOBAL PROFIT: ${(obj['rate'] - first_rate):,} {currency}")
+                print(f"[{updown}] CURRENT BALANCE  % {increase_rate_percentage:,}")
+                print(f"[{updown}] GLOBAL BALACE  % {first_rate_percentage:,}")
+                print('·········································································')
                 if increase_rate_percentage < 0 and min_rate is not None:
-                    if abs(abs(increase_rate_percentage) - abs(min_rate)) <= rate_window:
-                        print(f'\n=============== [!] Alert!!! BTC has DECREASED   % {increase_rate_percentage} ===============\n')
+                    if abs(abs(first_rate_percentage) - abs(min_rate)) <= rate_window:
+                        print(f'\n===== [!] Alert!!! BTC has DECREASED  % {first_rate_percentage:,} since {first_rate:,} =====\n')
                 if increase_rate_percentage > 0 and max_rate is not None:
-                    if abs(abs(increase_rate_percentage) - abs(min_rate)) <= rate_window:
-                        print(f'\n=============== [!] Alert!!! BTC has INCREASED   % {increase_rate_percentage} ===============\n')
+                    if abs(abs(first_rate_percentage) - abs(max_rate)) <= rate_window:
+                        print(f'\n===== [!] Alert!!! BTC has INCREASED  % {first_rate_percentage:,} since {first_rate:,}  =====\n')
+            previous_rate = obj['rate']
             previous_rate_percentage = increase_rate_percentage
             if times > -1:
                 if counter < times - 1:
